c523369d428ecd86379f627d31aee39749960ab5
c9s <cornelius.howl@gmail.com>
2012-01-28 19:19:07 +0800

    Add version info

--- src/MiniPear/Console.php
+++ src/MiniPear/Console.php
@@ -4,6 +4,8 @@ use CLIFramework\Application as ConsoleApplication;
 
 class Console extends ConsoleApplication
 {
+    const app_name = 'minipear';
+    const app_version = '0.0.2';
 
     function init()
     {
