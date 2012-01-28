eff4c9f994775c846d7f3545ffd7341bc57649c6
c9s <cornelius.howl@gmail.com>
2012-01-29 01:22:26 +0800

    comment.

--- tests/MiniPear/UtilsTest.php
+++ tests/MiniPear/UtilsTest.php
@@ -14,9 +14,9 @@ class UtilsTest extends PHPUnit_Framework_TestCase
         path_ok('test/rest/p/packages.xml');
         unlink('test/rest/p/packages.xml');
 
-        var_dump( version_compare( 'REST1.1' , 'REST1.2' ) );
-        var_dump( version_compare( 'REST1.1' , 'REST1.1' ) );
-        var_dump( version_compare( 'REST1.1' , 'REST1.0' ) );
+        // var_dump( version_compare( 'REST1.1' , 'REST1.2' ) );
+        // var_dump( version_compare( 'REST1.1' , 'REST1.1' ) );
+        // var_dump( version_compare( 'REST1.1' , 'REST1.0' ) );
     }
 }
 
