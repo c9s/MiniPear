1fc9d2056b2990de1823c8632aae94936c544cc4
Jean-Séb <jeanseb@au-fil-du.net>
2013-03-18 23:13:57 +0100

    Adding non regression test on UpdatePackage::setChannel

---
+++ tests/MiniPear/Progress/UpdatePackageTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use MiniPear\Progress\UpdatePackage;
+require 'Archive/Tar.php';
+class MiniPear_Progress_UpdatePackageTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var string
+     */
+    protected $packagePath;
+
+    protected function preparePackage($testArchive = 'package.tgz')
+    {
+        $this->packagePath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $testArchive;
+        copy(__DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . $testArchive, $this->packagePath);
+    }
+
+    protected function tearDown()
+    {
+        unlink($this->packagePath);
+    }
+
+    public function testSetChannelChangeTheChannelInPackageXml()
+    {
+        $this->preparePackage();
+        $channel = 'new.channel.net';
+        UpdatePackage::setChannel($this->packagePath, $channel);
+        $tar = new \Archive_Tar($this->packagePath);
+        $this->assertContains($channel, $tar->extractInString('package.xml'));
+    }
+}
